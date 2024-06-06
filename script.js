148a9cef0c0de1efcc27d5a5998a9da296d73c13
SaiRuthwik22 <160124679+SaiRuthwik22@users.noreply.github.com>
2024-06-06 13:48:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function stringChop(str, size) {
   // your code here
+	    let x = 0
+    let y = size
+    let arr = []
+    while(true){
+    if(y<str.length){
+        arr.push(str.slice(x,y))
+        x +=size
+        y+=size
+    }
+    else{
+        arr.push(str.slice(x,y))
+        break
+    }
+}
+return arr
 }
 
 // Do not change the code below
